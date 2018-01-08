4acd43a1f428520afa997605cffcf9dc754c5f21
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2018-01-08 15:40:34 +0100

    Add a log factory for the Lykke common logger

---
+++ QuickFIXn/Lykke/LykkeLogFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace QuickFix.Lykke
+{
+    /// <inheritdoc />
+    public sealed class LykkeLogFactory : ILogFactory
+    {
+        private readonly Common.Log.ILog _lykkeLog;
+        private readonly bool _logIncoming;
+        private readonly bool _logOutgoing;
+        private readonly bool _logEvent;
+
+        public LykkeLogFactory(Common.Log.ILog lykkeLog, bool logIncoming = true, bool logOutgoing = true, bool logEvent = true)
+        {
+            this._lykkeLog = lykkeLog;
+            _logIncoming = logIncoming;
+            _logOutgoing = logOutgoing;
+            _logEvent = logEvent;
+        }
+
+        /// <inheritdoc />
+        public ILog Create(SessionID sessionId)
+        {
+            return new LykkeQuickFixLog(_lykkeLog, sessionId, _logIncoming, _logOutgoing, _logEvent);
+        }
+    }
+}
---
+++ QuickFIXn/Lykke/LykkeQuickFixLog.cs
@@ -0,0 +1,52 @@
+﻿namespace QuickFix.Lykke
+{
+    internal sealed class LykkeQuickFixLog : ILog
+    {
+        private readonly Common.Log.ILog _lykkeLog;
+        private readonly SessionID _sessionId;
+        private readonly bool _logIncoming;
+        private readonly bool _logOutgoing;
+        private readonly bool _logEvents;
+
+        public LykkeQuickFixLog(Common.Log.ILog lykkeLog, SessionID sessionId, bool logIncoming, bool logOutgoing, bool logEvents)
+        {
+            _lykkeLog = lykkeLog;
+            _sessionId = sessionId;
+            _logIncoming = logIncoming;
+            _logOutgoing = logOutgoing;
+            _logEvents = logEvents;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Clear()
+        {
+        }
+
+        public void OnIncoming(string msg)
+        {
+            if (_logIncoming)
+            {
+                _lykkeLog.WriteInfoAsync(nameof(LykkeQuickFixLog), nameof(OnIncoming), _sessionId.ToString(), msg).Wait();
+            }
+        }
+
+        public void OnOutgoing(string msg)
+        {
+            if (_logOutgoing)
+            {
+                _lykkeLog.WriteInfoAsync(nameof(LykkeQuickFixLog), nameof(OnOutgoing), _sessionId.ToString(), msg).Wait();
+            }
+        }
+
+        public void OnEvent(string s)
+        {
+            if (_logEvents)
+            {
+                _lykkeLog.WriteInfoAsync(nameof(LykkeQuickFixLog), nameof(OnEvent), _sessionId.ToString(), s).Wait();
+            }
+        }
+    }
+}
